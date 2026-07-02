85c9810a3801f1ba322aa78fbb764205f2e4b770
bilal-baig-dev <bilalbaigvaservices@gmail.com>
2026-07-03 00:14:25 +0500

    chore: added password validator util function

---
+++ app/common/validators.py
@@ -0,0 +1,20 @@
+import re
+
+
+def validate_password(value: str) -> str:
+    if not re.search(r"[A-Z]", value):
+        raise ValueError(
+            "Password must contain at least one uppercase letter.")
+
+    if not re.search(r"[a-z]", value):
+        raise ValueError(
+            "Password must contain at least one lowercase letter.")
+
+    if not re.search(r"\d", value):
+        raise ValueError("Password must contain at least one number.")
+
+    if not re.search(r"[!@#$%^&*()_\-+=\[\]{}|\\:;\"'<>,.?/~`]", value):
+        raise ValueError(
+            "Password must contain at least one special character.")
+
+    return value
