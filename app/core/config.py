ae03664990b62dc07ca30251e527658e121297af
bilal-baig-dev <bilalbaigvaservices@gmail.com>
2026-07-10 23:09:17 +0500

    chore: addedd some env variable in configuration

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,14 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
 
+    SECRET_KEY: str
+
+    JWT_ALGORITHM: str
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
     model_config = SettingsConfigDict(
         env_file=".env",
         case_sensitive=True
