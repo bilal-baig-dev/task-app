2b946aafdf2cc1aa30c57aa62672d5bbf57dd68c
bilal-baig-dev <bilalbaigvaservices@gmail.com>
2026-07-10 23:48:03 +0500

    chore: export refresh token model

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,7 +1,9 @@
+from app.db.models.refresh_token import RefreshToken
 from app.db.models.task import Task
 from app.db.models.user import User
 
 __all__ = [
     "User",
-    "Task"
+    "Task",
+    "RefreshToken",
 ]
